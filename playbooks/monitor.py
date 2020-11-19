50073a547b349561be888f91fe2b3a7aa6cf911a
Qunfei Wu <qunfei.wu@allianz.com>
2020-11-19 14:07:36 +0100

    add you own kpi index

--- playbooks/monitor.py
+++ playbooks/monitor.py
@@ -1,6 +1,6 @@
 # https://faust.readthedocs.io/en/latest/userguide/sensors.html#sensor-api-reference
 import asyncio
-from typing import Mapping
+from typing import Mapping, Any
 
 import faust
 import ssl
@@ -9,7 +9,10 @@ from dotenv import load_dotenv
 import os
 import aiohttp
 import logging
-from faust import Worker
+from faust import Worker, Monitor
+from faust.types import AppT
+from mode.utils.types.trees import NodeT
+from prometheus_client import Counter
 
 from playbooks.prometheusMonitor import PrometheusMonitor
 
@@ -24,7 +27,25 @@ kafka_password = os.getenv("kafka_password")
 
 APP_NAME = 'monitor'
 
-app = faust.App(
+
+class MyMonitor(PrometheusMonitor):
+
+    def __init__(self, app: AppT, pattern: str = '/metrics', **kwargs: Any) -> None:
+        super().__init__(app, pattern, **kwargs)
+        self.kpi = Counter(
+            'kpi', 'business_kpi')
+
+    def on_customer_increase(self):
+        self.kpi.inc(1)
+
+
+class MyApp(faust.App):
+
+    def __init__(self, id: str, *, monitor: MyMonitor = None, config_source: Any = None, loop: asyncio.AbstractEventLoop = None, beacon: NodeT = None, **options: Any) -> None:
+        super().__init__(id, monitor=monitor, config_source=config_source, loop=loop, beacon=beacon, **options)
+
+
+app = MyApp(
     id=APP_NAME,
     broker=kafka_broker,
     broker_credentials=faust.SASLCredentials(
@@ -37,14 +58,15 @@ app = faust.App(
     topic_replication_factor=3
 )
 
-app.monitor = PrometheusMonitor(app, pattern='/metrics')
+app.monitor = MyMonitor(app, pattern='/metrics')
 
 livecheck = app.LiveCheck()
 
 
 @app.page("/customer")
 async def increase(web, request):
-    pass
+    app.monitor.on_customer_increase()
+    return "hi"
 
 
 if __name__ == '__main__':
