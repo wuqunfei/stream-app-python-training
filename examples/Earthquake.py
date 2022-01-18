376fc719bfd51f5ed612ccf80e4d29f4ff352d84
Qunfei Wu <qunfei.wu@allianz.com>
2022-01-18 16:05:18 +0100

    add earthquake

--- examples/Earthquake.py
+++ examples/Earthquake.py
@@ -19,6 +19,7 @@ app = faust.App(
 )
 
 
+# Schema Register Case
 class EarthQuakeRecord(faust.Record):
     value: int
 
@@ -26,6 +27,16 @@ class EarthQuakeRecord(faust.Record):
 topic = app.topic('earthquake', value_type=EarthQuakeRecord)
 
 
+# 1. get earthquake  http -> Produce message
+def get_earthquake():
+    pass
 
 
+# 2. Consume the message into database
+def persist_database():
+    pass
 
+
+# 3. Expose data into reset api service
+def api():
+    pass
