41b9d1cf3b1e7fe28fd7a6ef671c151e4aed6f9c
Qunfei Wu <qunfei.wu@allianz.com>
2022-01-25 19:44:18 +0100

    add sotck market example

--- examples/04.StockMarket.py
+++ examples/04.StockMarket.py
@@ -1,43 +1,78 @@
-# https://brokerchooser.com/broker-reviews/alpaca-trading-review
-# https://algotrading101.com/learn/alpaca-trading-api-guide/
-# https://manual.arcadetrader.com/
-# https://github.com/alpacahq/alpaca-backtrader-api
-# https://gbeced.github.io/pyalgotrade/docs/v0.20/html/index.html
-# https://github.com/kernc/backtesting.py/blob/master/doc/alternatives.md
-# stream -> public message -> backtest ->  Good -> Buy
-
+import logging
 import os
-import backtrader as bt
+from datetime import timedelta
+
 import alpaca_trade_api
-
+import faust
 from alpaca_trade_api.common import URL
+from faust import Worker
 
+app = faust.App(
+    'stock-app',
+    broker='kafka://localhost:9092',
+    value_serializer='json',
+    store='rocksdb://',
+    version=1,
+)
+
+ALPACA_BASE_URL = URL('https://paper-api.alpaca.markets')
+alpaca_ws_url = URL('wss://data.alpaca.markets')
 ALPACA_API_KEY = os.getenv('alpaca_key_id')
 ALPACA_SECRET_KEY = os.getenv('alpaca_secret_key')
-ALPACA_PAPER = True
 
-alpaca_base_url = URL('https://paper-api.alpaca.markets')
-alpaca_ws_url = URL('wss://data.alpaca.markets')
-alpaca_key_id = os.getenv('alpaca_key_id')
-alpaca_secret_key = os.getenv('alpaca_secret_key')
-
-api = alpaca_trade_api.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, alpaca_base_url, api_version='v2')
-
-aapl = api.get_barset('AAPL', 'day')
-
-data = aapl.df
+alpaca = alpaca_trade_api.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_BASE_URL, api_version='v2')
 
 
-class SmaCross(bt.SignalStrategy):
-    def __init__(self):
-        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
-        crossover = bt.ind.CrossOver(sma1, sma2)
-        self.signal_add(bt.SIGNAL_LONG, crossover)
+class OHLCRecord(faust.Record, serializer='json'):
+    Name: str
+    datetime: str
+
+    Open: float
+    High: float
+    Low: float
+    Close: float
+    Volume: float
 
 
-is_live = False
-data = bt.feeds.PandasData(dataname=data)
-cerebro = bt.Cerebro()
-cerebro.adddata(data)
-cerebro.addstrategy(SmaCross)
-cerebro.run()
+stock_market_topic = app.topic('stockmarket-topic', key_type=OHLCRecord, value_type=OHLCRecord, partitions=2)
+stock_market_table = app.Table(name='stockmarket-table', default=float, partitions=10).tumbling(
+    size=timedelta(seconds=10), expires=timedelta(seconds=60))
+
+stock_order_topic = app.topic('stock-order-topic', key_type=str, value_type=str, partitions=10)
+
+
+@app.timer(interval=1)
+async def get_ohlc():
+    FAANG_STOCKS = ['FB', 'AMZN', 'APPL', 'NFLX', 'GOOG']
+    df = alpaca.get_latest_bars(symbols=FAANG_STOCKS)
+    for name, bar in df.items():
+        record = OHLCRecord(
+            Name=name,
+            datetime=bar.t,
+
+            Open=bar.o,
+            Close=bar.c,
+            High=bar.h,
+            Low=bar.l,
+            Volume=bar.v
+        )
+        # logging.info(record)
+        await stock_market_topic.send(key=record, value=record)
+
+
+@app.timer(interval=10)
+async def back_test():
+    key = 'stock_name'
+    action = 'buy'
+    await stock_order_topic.send(key=key, value=action)
+
+
+@app.agent(stock_order_topic, concurrency=10, isolated_partitions=False)
+async def handle_order(orders):
+    async for key, order in orders.items():
+        logging.info(f'Send Order {key} to {order}')
+
+
+if __name__ == '__main__':
+    worker = Worker(app=app, loglevel=logging.INFO)
+    worker.execute_from_commandline()
