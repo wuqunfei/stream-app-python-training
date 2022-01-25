aaf4208095ca45642c66ac92f25d8d92688ca84c
Qunfei Wu <qunfei.wu@allianz.com>
2022-01-25 11:21:40 +0100

    add backtrader test code

--- examples/04.StockMarket.py
+++ examples/04.StockMarket.py
@@ -1,29 +1,43 @@
 # https://brokerchooser.com/broker-reviews/alpaca-trading-review
+# https://algotrading101.com/learn/alpaca-trading-api-guide/
+# https://manual.arcadetrader.com/
+# https://github.com/alpacahq/alpaca-backtrader-api
+# https://gbeced.github.io/pyalgotrade/docs/v0.20/html/index.html
+# https://github.com/kernc/backtesting.py/blob/master/doc/alternatives.md
+# stream -> public message -> backtest ->  Good -> Buy
+
 import os
-import datetime
-import alpaca_trade_api as tradeapi
+import backtrader as bt
+import alpaca_trade_api
+
 from alpaca_trade_api.common import URL
-from alpaca_trade_api.stream import Stream
+
+ALPACA_API_KEY = os.getenv('alpaca_key_id')
+ALPACA_SECRET_KEY = os.getenv('alpaca_secret_key')
+ALPACA_PAPER = True
 
 alpaca_base_url = URL('https://paper-api.alpaca.markets')
 alpaca_ws_url = URL('wss://data.alpaca.markets')
 alpaca_key_id = os.getenv('alpaca_key_id')
 alpaca_secret_key = os.getenv('alpaca_secret_key')
 
-stream = Stream(key_id=alpaca_key_id,
-                secret_key=alpaca_secret_key,
-                base_url=alpaca_base_url)
+api = alpaca_trade_api.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, alpaca_base_url, api_version='v2')
+
+aapl = api.get_barset('AAPL', 'day')
+
+type(aapl)
 
 
-async def trade_callback(t):
-    print('trade', t)
+class SmaCross(bt.SignalStrategy):
+    def __init__(self):
+        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
+        crossover = bt.ind.CrossOver(sma1, sma2)
+        self.signal_add(bt.SIGNAL_LONG, crossover)
 
 
-async def quote_callback(q):
-    print('quote', q)
-
-
-stream.subscribe_trades(trade_callback, 'AAPL')
-stream.subscribe_quotes(quote_callback, 'IBM')
-
-stream.run()
+is_live = False
+data = bt.feeds.PandasData(aapl.df)
+cerebro = bt.Cerebro()
+cerebro.adddata(data)
+cerebro.addstrategy(SmaCross)
+cerebro.run()
